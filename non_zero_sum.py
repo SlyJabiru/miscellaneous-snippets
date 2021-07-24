e2aed2e303fdfb218c2af91eb39702c4204d9fb4
SlyJabiru <gcp.study.sjlee@gmail.com>
2021-07-24 15:21:20 +0900

    Corrected maximin and minimax algorithms

--- non_zero_sum.py
+++ non_zero_sum.py
@@ -1,37 +1,86 @@
 import numpy as np
 
 
+def non_zero_sum_maximin(matrix):
+    """
+    Apply maximin method to non zero sum payoff matrix.
+    ui_ = max_{i} min_{-i} ui(a{i}, a{-i})
+
+    :param matrix: payoff matrix with shape (2, _, _)
+    :return:
+    """
+
+    # Example
+    # Prisoner's Dilemma
+    #  (5, 5),  (0, 10)
+    # (10, 0), (3,  3)
+
+    # First, consider player0's payoff
+    #   5, 0 | 0
+    #  10, 3 | 3
+    u0 = matrix[0]
+    u0_min_by_1 = np.min(u0, axis=1)  # min_{-0} u0(a{0}, a{-0})
+    u0_max_among_mins = np.max(u0_min_by_1)
+    u0_best_strategy = np.argmax(u0_min_by_1)
+    print(f'u0_max_among_mins: {u0_max_among_mins}')
+    print(f'u0_best_strategy: {u0_best_strategy}')
+
+    # Let's consider player1's payoff
+    # 5, 10
+    # 0,  3
+    # -----
+    # 0,  3
+    u1 = matrix[1]
+    u1_min_by_0 = np.min(u1, axis=0)  # min_{-1} u1(a{1}, a{-1})
+    u1_max_among_mins = np.max(u1_min_by_0)
+    u1_best_strategy = np.argmax(u1_min_by_0)
+    print(f'u1_max_among_mins: {u1_max_among_mins}')
+    print(f'u1_best_strategy: {u1_best_strategy}')
+
+
 def non_zero_sum_minimax(matrix):
     """
     Apply minimax method to non zero sum payoff matrix
+    ui^_ = min_{-i} max_{i} ui(a{i}, a{-i})
     
     :param matrix: 
     :return: 
     """
     # Example
     # Prisoner's Dilemma
-    #  (3,3),  (10, 0)
-    # (0, 10), (5,  5)
+    #  (5, 5),  (0, 10)
+    # (10, 0), (3,  3)
 
-    # Player 1 can expect that player 2 should choose the strategy 0
-    # Because the possible minimum payoff is larger for player 2 when she choose the strategy 0
+    # First, consider player0's payoff
+    #   5, 0
+    #  10, 3
+    # ------
+    #  10, 3
+    u0 = matrix[0]
+    u0_max_by_1 = np.max(u0, axis=0)
+    u0_min_among_maxs = np.min(u0_max_by_1)  # 0 was disturbed by other players
+    u0_resulted_strategy = np.argmin(u0_max_by_1)
+    print(f'u0_min_among_maxs: {u0_min_among_maxs}')
+    print(f'u0_resulted_strategy: {u0_resulted_strategy}')
 
-    player2_worst = np.min(arr[1])
-    player2_expected_strategy = np.argmax(player2_worst)
-
-    player1_expected_array = arr[0][:, player2_expected_strategy]
-    player1_best_payoff = np.max(player1_expected_array)
-    player1_best_strategy = np.argmax(player1_expected_array)
-
-    return player1_best_payoff, player1_best_strategy
+    # First, consider player0's payoff
+    #  5, 10 | 10
+    #  0,  3 | 3
+    u1 = matrix[1]
+    u1_max_by_0 = np.max(u1, axis=1)
+    u1_min_among_maxs = np.min(u1_max_by_0)
+    u1_resulted_strategy = np.argmin(u1_max_by_0)
+    print(f'u1_min_among_maxs: {u1_min_among_maxs}')
+    print(f'u1_resulted_strategy: {u1_resulted_strategy}')
 
 
 if __name__ == '__main__':
-    arr = np.array(
-        [[[3, 10],
-         [0, 5]],
-        [[3, 0],
-         [10, 5]]]
-    )
+    arr = np.array([
+        [[5, 0],
+         [10, 3]],
+        [[5, 10],
+         [0, 3]]
+    ])
 
-    print(non_zero_sum_minimax(arr))
+    non_zero_sum_maximin(arr)
+    non_zero_sum_minimax(arr)
--- zero_sum_minimax.py
+++ zero_sum_minimax.py
@@ -1,63 +1,115 @@
 import numpy as np
 
 
-def zero_sum_minimax(arr):
-    """
-    Find the equilibrium in a discrete zero sum 2 player game using minimax method
-
-    :param arr: A numpy array with 2 dimension. Only contains numbers in the perspective of the player 1
-    :return: An index of strategy the player 1 choose, utility / payoff the player 1 gets
-    """
-
-    # In a discrete 2 player zero sum game
-    # player 1's strategy
-
-    # Player 1 can consider that
-    # Player 2 will choose the strategy
-    # that makes the least loss in the worst case
-
-    #      12  -1  0
-    #       5   2  3
-    #     -16   0 -1
-    # --------------
-    #      12   2  3  will be the worst case in the perspective of player 2,
-    #                 if player 2 selects A B C respectively.
-    #                 Then player 1 can assume that player 2 will choose the strategy B
-    player_2_worst = np.max(arr, axis=0)
-    player_2_predicted_strategy = np.argmin(player_2_worst)
-    player_2_predicted_payoff_arr = arr[:, player_2_predicted_strategy]
-
-    # Then player 1 maximize its utility / payoff
-    player_1_strategy = np.argmax(player_2_predicted_payoff_arr)
-    player_1_value = player_2_predicted_payoff_arr[player_1_strategy]
-
-    return player_1_strategy, player_1_value
+# If we consider all possible cases, firstly consider the first min/max operation!
+# maximin: ui_ = max_{i} min_{-i} ui(ai, a{-i})  -> first consider  i's action
+# minimax: ui^_ = min_{-i} max_{i} ui(ai, a{-i}) -> first consider -i's action
 
 
 def zero_sum_maximin(arr):
     """
     Find the equilibrium in a discrete zero sum 2 player game using maximin method
+    ui_ = max_{i} min_{-i} ui(ai, a{-i})
+    Maximize the minimum payoff by other players.
 
-    :param arr: A numpy array with 2 dimension. Only contains numbers in the perspective of the player 1
-    :return: An index of strategy the player 2 choose, utility / payoff the player 2 gets
+    :param arr: A numpy array with 2 dimension. Only contains numbers in the perspective of the row player
+    :param row_or_col:
+    :return:
     """
+
+    # In a discrete 2 player zero sum game
+
+    # row player. U_row = max_{row} min_{col} u_row(a_row, a_col)
+    # In the perspective of row player, let's put all possible action first
+    # Then, for each action, minimum payoff can be calculated
+    # Then among them list of min_per_action values, pick maximum!
 
     #      12  -1  0 |  -1
     #       5   2  3 |   2
     #     -16   0 -1 | -16
 
-    #                 will be the worst case in the perspective of player 1,
-    #                 if player 1 selects A B C respectively.
-    #                 Then player 2 can assume that player 1 will choose the strategy B
-    player_1_worst = np.min(arr, axis=1)
-    player_1_predicted_strategy = np.argmax(player_1_worst)
-    player_1_predicted_payoff_arr = arr[player_1_predicted_strategy]
+    # first, row player consider worst case per each action
+    row_worst_case = np.min(arr, axis=1)
+    row_max_among_mins = np.max(row_worst_case)
+    row_best_strategy = np.argmax(row_worst_case)
+    print(f'row_max_among_mins: {row_max_among_mins}')
+    print(f'row_best_strategy: {row_best_strategy}')
 
-    # Then player 2 minimize the opponent's utility / payoff
-    player_2_strategy = np.argmin(player_1_predicted_payoff_arr)
-    player_2_value = player_1_predicted_payoff_arr[player_2_strategy]
+    # column player's payoff matrix
+    #    -12   1  0
+    #     -5  -2 -3
+    #     16   0  1
+    # --------------
+    #    -12  -2 -3
+    col_worst_case = np.min(-arr, axis=0)
+    col_max_among_mins = np.max(col_worst_case)
+    col_best_strategy = np.argmax(col_worst_case)
+    print(f'col_max_among_mins: {col_max_among_mins}')
+    print(f'col_best_strategy: {col_best_strategy}')
 
-    return player_2_strategy, player_2_value
+    # Let's consider column player while not flipping the sign of original matrix
+    #      12  -1  0
+    #       5   2  3
+    #     -16   0 -1
+    # --------------
+    #      12   2  3
+    # The column player wants his lost should be small
+
+    col_max_lost = np.max(arr, axis=0)
+    col_min_among_max_lost = np.min(col_max_lost)
+    col_best_strategy = np.argmin(col_max_lost)
+    print(f'col_min_among_max_lost: {col_min_among_max_lost}')
+    print(f'col_best_strategy: {col_best_strategy}')
+
+
+def zero_sum_minimax(arr):
+    """
+    Find the equilibrium in a discrete zero sum 2 player game using maximin method
+    ui^_ = min_{-i} max_{i} ui(ai, a{-i})
+
+    :param arr: A numpy array with 2 dimension. Only contains numbers in the perspective of the row player
+    :param row_or_col:
+    :return:
+    """
+
+    # In a discrete 2 player zero sum game
+
+    # row player. U_row = min_{col} max_{row} u_row(a_row, a_col)
+    # Let's consider col player's action and consider that cases
+
+    #      12  -1  0
+    #       5   2  3
+    #     -16   0 -1
+    # --------------
+    #      12   2  3; If col plays 1 -> row's max will be 12, and then so on.
+    row_best_by_col_action = np.max(arr, axis=0)
+    row_min_among_maxs = np.min(row_best_by_col_action)
+    row_resulted_strategy = np.argmin(row_best_by_col_action)
+    print(f'row_min_among_maxs: {row_min_among_maxs}')
+    print(f'row_resulted_strategy: {row_resulted_strategy}')
+
+    # col player. Let's put other player's action first.
+    #     -12   1  0 |  1
+    #      -5  -2 -3 | -2
+    #      16   0  1 | 16
+    # Column player plans the best action for each action of the other player (the row player)
+    # However, the row player disturb this. Because he is adversary to the col player!
+    col_best_by_row_action = np.max(-arr, axis=1)
+    col_min_among_maxs = np.min(col_best_by_row_action)  # disturbed by other player
+    col_resulted_strategy = np.argmin(col_best_by_row_action)
+    print(f'col_min_among_maxs: {col_min_among_maxs}')
+    print(f'col_resulted_strategy: {col_resulted_strategy}')
+
+    # col player, but do not flip the sign
+    #      12  -1  0 |  -1
+    #       5   2  3 |   2
+    #     -16   0 -1 | -16
+    # Column player's best plan is, to minimize the lost for each action of the row player.
+    col_best_by_row_action = np.min(arr, axis=1)
+    col_max_among_min_losts = np.max(col_best_by_row_action)  # disturbed by other player
+    col_resulted_strategy = np.argmax(col_best_by_row_action)
+    print(f'col_max_among_min_losts: {col_max_among_min_losts}')
+    print(f'col_resulted_strategy: {col_resulted_strategy}')
 
 
 if __name__ == '__main__':
@@ -67,8 +119,12 @@ if __name__ == '__main__':
          [7, 5, 1, 3],
          [0, 8, -4, 5]]
     )
-    player_1_strategy, player_1_value = zero_sum_minimax(arr)
-    player_2_strategy, player_2_value = zero_sum_maximin(arr)
 
-    print(player_1_strategy, player_1_value)
-    print(player_2_strategy, player_2_value)
+    # arr = np.array([
+    #     [12, -1, 0],
+    #     [5, 2, 3],
+    #     [-16, 0, -1]
+    # ])
+    zero_sum_maximin(arr)
+    print()
+    zero_sum_minimax(arr)
